3b43db643fd27bfdf6d6b2ee5e8ef412e00cb20e
CHJMCMXCIII <73843393+CHJMCMXCIII@users.noreply.github.com>
2022-08-11 17:52:00 +0900

    센서 차트 컴포넌트 작업 (D3)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <SensorChart />
+    <div>
+
+    </div>
 </template>
 
 <script>
 import SensorChart from './components/SensorChart.vue'
+import {
+    ref,
+    onMounted,
+    computed,
+    inject
+} from 'vue'
+import store from './store'
+export default { 
 
-export default {
-  name: 'App',
-  components: {
-    SensorChart
-  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend/src/components/SensorChart.vue
+++ frontend/src/components/SensorChart.vue
@@ -23,6 +23,153 @@ export default {
         const type = computed(() => props.type)
         const sensors = computed(() => store.getters['sensors'])
         const sensor = computed(() => store.getters['sensor'][type.value])
+        
+        const setAreaAndScale = (key) => {
+            svg = d3.select(`.Chart-${key}`).append("svg")  
+                .attr("width", chartWidth + chartMargin.left + chartMargin.right)
+                .attr("height", chartHeight + chartMargin.top + chartMargin.bottom)
+                .append("g")
+                .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`)
+
+            xScale = d3.scaleTime().range([0, chartWidth])
+            yScale = d3.scaleLinear().range([chartHeight, 0])
+
+            xAxis = d3.axisBottom(xScale).tickFormat(timeFormat)
+            yAxis = d3.axisLeft(yScale)
+
+            area = d3.area()
+                .curve(d3.curveMonotoneX)
+                .x(d => xScale(d.time))
+                .y0(yScale(0))
+                .y1(d => yScale(d[key]))
+
+            line = d3.line().x(d => xScale(d.time)).y(d => yScale(d[key]))
+                .curve(d3.curveMonotoneX)
+            
+            tooltip = d3.select(`.tooltip`)
+        }
+
+        const initDraw = (data, key) => {
+            // data는 string형태로 오기 때문에 여기서 new Date 객체로 바꿔주어야 한다.
+            data.forEach(d => d.time = new Date(d.time))
+            xScale.domain(d3.extent(data, d => d.time))
+            const mn = d3.min(data, d => d[key])
+            const mx = d3.max(data, d => d[key])
+            yScale.domain([mn - chartMargin.value, mx + chartMargin.value])
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", `translate(0, ${chartHeight})`)
+                .call(xAxis)
+            svg.append("g")
+                .attr("class", "y axis")
+
+            svg.append("path")
+                .datum(data)
+                .attr("fill", "steelblue")
+                .attr("d", area)
+                .attr("class", "area")
+
+            // data를 통해 path를 그리는 3가지 방법
+            // svg.append("path").datum(data).attr("d", line)
+            // svg.append("path").data([data]).attr("d", line)
+            svg.append("path")
+                .attr("d", line(data))
+                .attr("class", "line")
+
+            circle = svg.selectAll("dot")
+                .data(data)
+                .enter().append("circle")
+                .attr("r", 3)
+                .on("mouseover", (event, d) => {
+                    console.log(event)
+                    console.log(key)
+                    tooltip.transition()
+                        .duration(200)
+                        .style("opacity", 1)
+                    const content = `<p>${props.text}</p> <p>[${timeFormat(d.time)}</p><h2>${d[key]}</h2>]`
+
+                    tooltip
+                        .html(content)
+                        .style("left", (event.pageX) - 83 + "px")
+                        .style("top", (event.pageY) - 130 + "px")
+                })
+                .on("mouseout", () => {
+                    tooltip.transition()
+                        .duration(200)
+                        .style("opacity", 0)
+                })
+
+            circle
+                .attr("cx", d => xScale(d.time))
+                .attr("cy", d => yScale(d[key]))
+        }
+
+        // draw에서는 데이터 처리가 아닌 data를 통해서 차트를 그리는 것에 집중해야 함.
+        const draw = (data, key) => {
+            data.forEach(d => d.time = new Date(d.time))
+
+            const mn = d3.min(data, d => d[key])
+            const mx = d3.max(data, d => d[key])
+            xScale.domain(d3.extent(data, d => d.time))
+            yScale.domain([mn - chartMargin.value, mx + chartMargin.value])
+
+            svg.select(".area")
+                .transition()
+                .duration(750)
+                .attr("d", area(data))
+
+            svg.select(".line")
+                .transition()
+                .duration(750)
+                .attr("d", line(data))
+
+            svg.select(".x.axis")
+                .transition()
+                .duration(750)
+                .call(xAxis)
+
+            svg.select(".y.axis")
+                .transition()
+                .duration(750)
+                .call(yAxis)
+
+            circle
+                .data(data)
+                .transition()
+                .duration(750)
+                .attr("cx", d => xScale(d.time))
+                .attr("cy", d => yScale(d[key]))
+        }
+
+        onMounted(() => {
+            setAreaAndScale(type.value)
+            let flag = false
+            store.subscribe((mutation) => {
+                if (mutation.type === "CHANGE_SENSOR_CHART") {
+                    if(!flag && sensors.value.length === 10) {
+                        initDraw(sensors.value, type.value)
+                        flag = true
+                    } else if(flag) {
+                        draw(sensors.value, type.value)
+                    }
+                }
+            })
+        })
+        
+
+        return {
+            sensors,
+            sensor,
+            svg,
+            xScale,
+            yScale,
+            xAxis,
+            yAxis,
+            line,
+            tooltip,
+            circle
+        }
 
     }
 }
